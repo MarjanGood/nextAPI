5bb98e64cd891a5a730f6a4b0146b9697320ccf5
Goodarzyar <goodarzyar@ifb.ir>
2023-03-26 12:56:59 +0430

    Session119: Create model

---
+++ src/models/User.js
@@ -0,0 +1,9 @@
+import {schema , model, models} from "mongoose"
+
+const userSchema = new Schema({
+    name: String,
+})
+
+const User = models.User || model("User", userSchema);
+
+export default User;
